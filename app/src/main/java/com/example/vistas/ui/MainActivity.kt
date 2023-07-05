278f8258d2dd4d15a23309de8ce50718ea063c78
Bruno <76050939+BrunoMF99@users.noreply.github.com>
2023-07-04 22:28:07 -0500

    Add files via upload

--- app/src/main/java/com/example/vistas/ui/MainActivity.kt
+++ app/src/main/java/com/example/vistas/ui/MainActivity.kt
@@ -1,103 +1,53 @@
-package com.example.vistas.ui
+package com.example.scannerqr
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
-import com.example.vistas.R
-import com.example.vistas.io.ApiService
-import com.example.vistas.io.response.LoginResponse
-import com.example.vistas.model.userDTO
-import com.example.vistas.util.Prefs
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
+import com.example.scannerqr.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
-        lateinit var prefs: Prefs
-    }
-
-    private val apiService : ApiService by lazy{
-        ApiService.create()
-    }
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fr_inicio)
-        prefs = Prefs(applicationContext)
-
-
-        val botonLogin: Button = findViewById(R.id.btnIng)
-        val botonReg: Button = findViewById(R.id.btnReg)
-
-        botonLogin.setOnClickListener {
-            performLogin()
-        }
-
-        botonReg.setOnClickListener {
-            gotoRegistro()
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnScanner.setOnClickListener { initScanner() }
     }
 
+    private fun initScanner(){
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("Scan QR Code")
+        integrator.initiateScan()
+    }
 
-    private fun performLogin(){
-        val user_text = findViewById<EditText>(R.id.edtUser).text.toString()
-        val pass_text = findViewById<EditText>(R.id.editPass).text.toString()
-
-        if(user_text.isNotEmpty() && pass_text.isNotEmpty() ){
-            val cliente = userDTO(user_text,pass_text)
-            val call = apiService.postLogin(cliente)
-            call.enqueue(object: Callback<LoginResponse> {
-                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-
-                    if (response.isSuccessful){
-
-                        val loginResponse = response.body()
-
-                        if(loginResponse == null){
-                            Toast.makeText(applicationContext,"se produjo un error en el servidor",Toast.LENGTH_LONG).show()
-                            return
-                        }
-
-                        if(loginResponse.datos.id_cliente != null){
-                            Toast.makeText(applicationContext,"sesion iniciada correctamente",Toast.LENGTH_LONG).show()
-                            println(loginResponse)
-                            prefs.saveId(loginResponse.datos.id_cliente)
-                            prefs.saveName(loginResponse.datos.nombre)
-                            prefs.saveUser(loginResponse.datos.usuario)
-                            prefs.saveToken(loginResponse.datos.token)
-
-                            val intent = Intent(applicationContext, Inicio::class.java)
-                            startActivity(intent)
-                        }else{
-                            Toast.makeText(applicationContext,loginResponse.mensaje,Toast.LENGTH_SHORT).show()
-                        }
-                    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                try {
+                    val intent = Intent(this, Resultado2QRActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
-
-                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                    //manejo del error
+            } else {
+                try {
+                    val intent = Intent(this, ResultadoQRActivity::class.java)
+                    intent.putExtra("INTENT_NAME", result.contents)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
-
-            })
-        }else{
-            showErrorName()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
-
     }
 
-    private fun showErrorName(){
-        Toast.makeText(this, "Todos los campos deben ser llenados", Toast.LENGTH_LONG).show()
-    }
-
-    private fun gotoRegistro(){
-        val intentRegistro = Intent(applicationContext, Registro::class.java)
-        startActivity(intentRegistro)
-    }
 }
---
+++ app/src/main/java/com/example/vistas/ui/ResultadoQRActivity.kt
@@ -0,0 +1,32 @@
+package com.example.scannerqr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Button
+
+
+class ResultadoQRActivity : AppCompatActivity() {
+    //@SuppressLint("WrongViewCast")
+
+    lateinit var txtResultado: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fr_reserva_mesa_fecha)
+        txtResultado = findViewById(R.id.txtResultado)
+        val btnVolver1: Button = findViewById(R.id.btnVolver)
+        getAndShowName()
+        btnVolver1.setOnClickListener { onBackPressed() }
+    }
+
+    //@SuppressLint("WrongViewCast")
+    private fun getAndShowName() {
+        val bundle = intent.extras
+        val name = bundle?.get("INTENT_NAME")
+        //var etResult: EditText = findViewById(R.id.txtResultado)
+        //val cont = name.toString()
+        txtResultado.text = "$name"
+
+    }
+}
